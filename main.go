e98af5ba6510bd6366dfc7f04f67e7e85a9dce7c
Oscar Salcedo <josueht15@gmail.com>
2024-02-06 20:20:01 -0500

    feat: Balanceo de carga

--- main.go
+++ main.go
@@ -8,27 +8,34 @@ import (
 )
 
 func main() {
-	//var root *node.Node
-	//root = node.Add(root, 5)
-	//root = node.Add(root, 2)
-	//root = node.Add(root, 1)
-	//root = node.Add(root, 7)
-	//root = node.Add(root, 4)
-	//root = node.Add(root, 10)
-	//
-	//root.Print(root)
-
 	tree := node.New(ordered.NewOrderedInt())
+	tree.Add(15)
 	tree.Add(5)
 	tree.Add(4)
+	tree.Add(2)
+	tree.Add(20)
+	tree.Add(11)
+	tree.Add(9)
+	tree.Add(25)
+	tree.Add(35)
+	tree.Add(7)
 	tree.Add(6)
+	tree.Add(3)
 	tree.Add(10)
 	tree.Add(1)
+	tree.Add(16)
+	tree.Add(21)
+	tree.Add(22)
+	tree.Add(23)
 
-	tree.Print()
+	tree.PrintTree()
+	fmt.Printf("\n=========================\n")
 
 	tree.Delete(5)
-	fmt.Println()
+	tree.Delete(1)
+	tree.Delete(2)
+	tree.Delete(4)
+	tree.Delete(21)
 
-	tree.Print()
+	tree.PrintTree()
 }
--- node/node.go
+++ node/node.go
@@ -23,6 +23,28 @@ func New(ordered ordered.Ordered) Tree {
 	return Tree{ordered: ordered}
 }
 
+func (t *Tree) PrintTree() {
+	t.printTree(t.root, 0)
+}
+
+func (t *Tree) printTree(root *Node, spacing int) {
+	if root == nil {
+		return
+	}
+
+	spacing += 5
+
+	t.printTree(root.right, spacing)
+
+	fmt.Println()
+	for i := 5; i < spacing; i++ {
+		fmt.Print(" ")
+	}
+	fmt.Printf("%d", root.data)
+
+	t.printTree(root.left, spacing)
+}
+
 func (t *Tree) Add(value any) {
 	t.root = t.add(t.root, value)
 }
@@ -46,44 +68,102 @@ func (t *Tree) add(current *Node, data any) *Node {
 	// one is for the current node
 	current.height = 1 + max(height(current.left), height(current.right))
 
-	//balance := calculateBalance(current)
+	current = balancer(current)
 
 	return current
+}
+
+func balancer(node *Node) *Node {
+	balance := calculateBalance(node)
+
+	// left - left
+	if balance > 1 && calculateBalance(node.left) >= 0 {
+		return rotateRight(node)
+	}
+
+	// left - right
+	if balance > 1 && calculateBalance(node.left) < 0 {
+		node.left = rotateLeft(node.left)
+		return rotateRight(node)
+	}
+
+	// right - right
+	if balance < -1 && calculateBalance(node.right) <= 0 {
+		return rotateLeft(node)
+	}
+
+	// right - left
+	if balance < -1 && calculateBalance(node.right) > 0 {
+		node.right = rotateRight(node.right)
+		return rotateLeft(node)
+	}
+
+	return node
+}
+
+func rotateRight(node *Node) *Node {
+	childLeft := node.left
+	childRightOfLeft := childLeft.right
+
+	childLeft.right = node
+	node.left = childRightOfLeft
+
+	node.height = max(height(node.left), height(node.right)) + 1
+	childLeft.height = max(height(childLeft.left), height(childLeft.right)) + 1
+
+	return childLeft
+}
+
+func rotateLeft(node *Node) *Node {
+	childRight := node.right
+	childLeftOfRight := childRight.left
+
+	childRight.left = node
+	node.right = childLeftOfRight
+
+	node.height = max(height(node.left), height(node.right)) + 1
+	childRight.height = max(height(childRight.left), height(childRight.right)) + 1
+
+	return childRight
 }
 
 func (t *Tree) Delete(data any) {
 	t.root = t.delete(t.root, data)
 }
 
-func (t *Tree) delete(n *Node, data any) *Node {
-	if n == nil {
-		return n
+func (t *Tree) delete(current *Node, data any) *Node {
+	if current == nil {
+		return nil
 	}
 
-	if t.ordered.IsEqual(n.data, data) {
+	if t.ordered.IsEqual(current.data, data) {
 		// ifs replace actual node whit opposite node, opposite node can be nil
-		if n.left == nil {
-			return n.right
+		if current.left == nil {
+			return current.right
 		}
 
-		if n.right == nil {
-			return n.left
+		if current.right == nil {
+			return current.left
 		}
 
 		// here use right for no break the tree
-		minRight := t.findMin(n.right)
-		n.data = minRight.data
+		minRight := t.findMin(current.right)
+		current.data = minRight.data
 
-		n.right = t.delete(n.right, minRight.data)
-
-		return n
+		current.right = t.delete(current.right, minRight.data)
 	}
 
-	if t.ordered.IsLeft(n.data, data) {
-		return t.delete(n.left, data)
+	if t.ordered.IsLeft(current.data, data) {
+		current.left = t.delete(current.left, data)
+	} else {
+		current.right = t.delete(current.right, data)
 	}
 
-	return t.delete(n.right, data)
+	current.height = 1 + max(height(current.left), height(current.right))
+
+	current = balancer(current)
+
+	return current
 }
 
 func (t *Tree) findMin(n *Node) *Node {
